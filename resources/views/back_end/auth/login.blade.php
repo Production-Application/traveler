b8e5e8a8d3b36b55df4c709d8c8227256a0fc85c
mukhulaazam <mukhulaazam@gmail.com>
2020-05-09 13:17:07 +0600

    Login view added and next is registration and forget and verify

--- resources/views/back_end/auth/login.blade.php
+++ resources/views/back_end/auth/login.blade.php
@@ -1,1 +1,76 @@
-<?php
+<!DOCTYPE html>
+<html class="no-js " lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+    <title>:: Hello! Admin :: Login Page</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="{{ asset('back_end/assets/plugins/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('back_end/assets/css/style.min.css') }}">
+</head>
+
+<body class="theme-blush">
+
+<div class="authentication">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-12">
+                <form class="card auth_form">
+                    <div class="header">
+                        <img class="logo" src="{{ asset('back_end/assets/images/logo.svg') }}" alt="">
+                        <h5>Log in</h5>
+                    </div>
+                    <div class="body">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Username">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Password">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><a href="forgot-password.html" class="forgot" title="Forgot Password"><i class="zmdi zmdi-lock"></i></a></span>
+                            </div>
+                        </div>
+                        <div class="checkbox">
+                            <input id="remember_me" type="checkbox">
+                            <label for="remember_me">Remember Me</label>
+                        </div>
+                        <a href="index.html" class="btn btn-primary btn-block waves-effect waves-light">SIGN IN</a>
+                        <div class="signin_with mt-3">
+                            <p class="mb-0">or Sign Up using</p>
+                            <button class="btn btn-primary btn-icon btn-icon-mini btn-round facebook"><i class="zmdi zmdi-facebook"></i></button>
+                            <button class="btn btn-primary btn-icon btn-icon-mini btn-round twitter"><i class="zmdi zmdi-twitter"></i></button>
+                            <button class="btn btn-primary btn-icon btn-icon-mini btn-round google"><i class="zmdi zmdi-google-plus"></i></button>
+                        </div>
+                    </div>
+                </form>
+                <div class="copyright text-center">
+                    &copy;
+                    <script>document.write(new Date().getFullYear())</script>
+                    <span><a href="templatespoint.net">Traveler</a></span>
+                </div>
+            </div>
+            <div class="col-lg-8 col-sm-12">
+                <div class="card noBorderS">
+                    <img src="{{ asset('back_end/assets/images/signin.svg') }}" alt="Sign In"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Jquery Core Js -->
+<script src="{{ asset('back_end/assets/bundles/libscripts.bundle.js') }}"></script>
+<script src="{{ asset('bcak_end/assets/bundles/vendorscripts.bundle.js') }}"></script> <!-- Lib Scripts Plugin Js -->
+</body>
+
+
+</html>
--- routes/admin.php
+++ routes/admin.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('login',function (){
+   return view('back_end.auth.login');
+});
+
 Route::get('/','DashboardController@showDashboard')->name('dashboard');
 
 // Category section routes
